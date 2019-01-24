2f75a824775a225ab7459f809670d2ce0ef48bbc
Ninio <pereriksson98@hotmail.com>
2019-01-24 21:55:05 +0100

    testingstuff

--- RPGtest/Assets/Player.cs
+++ RPGtest/Assets/Player.cs
@@ -5,24 +5,29 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     private Vector2 dir;
     public float speed = 10;
+    private int lastDir = 2;
 
     private void Update()
     {
         if (Input.GetAxisRaw("Horizontal") > 0)
         {
             dir = Vector2.right;
+            lastDir = 1;
         }
         else if (Input.GetAxisRaw("Horizontal") < 0)
         {
             dir = Vector2.left;
+            lastDir = 3;
         }
         else if (Input.GetAxisRaw("Vertical") > 0)
         {
             dir = Vector2.up;
+            lastDir = 0;
         }
         else if (Input.GetAxisRaw("Vertical") < 0)
         {
             dir = Vector2.down;
+            lastDir = 2;
         }
         else
         {
