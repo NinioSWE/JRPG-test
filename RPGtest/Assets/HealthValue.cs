46fce731492c89fa78d06f0e122fc34d6e4f0694
Hamplox <zorkan.p@gmail.com>
2019-01-24 22:33:13 +0100

    UI Combat

---
+++ RPGtest/Assets/HealthValue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthValue : MonoBehaviour {
+    
+    public float health = 100;
+    public Text healthText;
+
+    private float startHealth;
+    private Scrollbar m_Scrollbar;
+
+    private void Start()
+    {
+        startHealth = health;
+        m_Scrollbar = GetComponent<Scrollbar>();
+    }
+    void Update () {
+        if(health > startHealth)
+        {
+            health = startHealth;
+        }
+        m_Scrollbar.size = health/startHealth;
+        healthText.text = health + "/" + startHealth;
+	}
+}
