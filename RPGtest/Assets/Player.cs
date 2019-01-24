690a106ae66aa57a6caa8a3ab701f7978e48e150
Hamplox <zorkan.p@gmail.com>
2019-01-24 21:21:08 +0100

    Sak

---
+++ RPGtest/Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private Vector2 dir;
+    public float speed = 10;
+
+    private void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            dir = Vector2.right;
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            dir = Vector2.left;
+        }
+        else if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            dir = Vector2.up;
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            dir = Vector2.down;
+        }
+        else
+        {
+            dir = Vector2.zero;
+        }
+        transform.position = transform.position + (Vector3)(dir * Time.deltaTime * speed);
+    }
+
+}
