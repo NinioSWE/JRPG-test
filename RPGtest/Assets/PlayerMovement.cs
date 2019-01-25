4685ec792a778d8a24fce9286007bffb6e05f193
FuyukiAndo <47009923+FuyukiAndo@users.noreply.github.com>
2019-01-25 01:11:55 +0100

    movement2

--- RPGtest/Assets/PlayerMovement.cs
+++ RPGtest/Assets/PlayerMovement.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour 
 {
 	[SerializeField] private float movementSpeed = 3.0f;
+	[SerializeField] private float accelerationSpeed = 1.5f;
+	[SerializeField] private float height = 4.0f;
+	[SerializeField] private float fallDelay  = .4f;
 	private float horizontalInput;
 	private float verticalInput;
+	private float jumpInput;
+	private float gravity;
+	private float velocity;
 	
 	private float left = 1;
 	private float right = -1;
@@ -15,20 +21,32 @@ public class PlayerMovement : MonoBehaviour
 	
 	Vector3 movement;
 
+	public static PlayerMovement playerReference;
+
 	void Start () 
 	{
+		//setup gravity
+		gravity = -(2*height)/Mathf.Pow(fallDelay, 2);
+		velocity = Mathf.Abs(gravity) * fallDelay;
 		
+		
+
+		playerReference = this;
+		//movement = gameObject.transform.position;
 	}
 	
 	void playerInput()
 	{
 		horizontalInput = Input.GetAxisRaw("Horizontal");
-		verticalInput = Input.GetAxisRaw("Vertical");	
+		verticalInput = Input.GetAxisRaw("Vertical");
+		jumpInput = Input.GetAxisRaw("Jump");
 	}
 	
 	void move(float dirX, float dirY, float dirZ)
-	{
-		transform.Translate(new Vector3(dirX * movementSpeed * Time.deltaTime, dirY * movementSpeed * Time.deltaTime, dirZ * movementSpeed * Time.deltaTime));
+	{		
+		transform.Translate(new Vector3(dirX * movementSpeed * Time.deltaTime, 
+										dirY * velocity * Time.deltaTime * 3f, 
+										dirZ * movementSpeed * Time.deltaTime * 3f));
 	}
 	
 	void playerMovement()
@@ -57,8 +75,21 @@ public class PlayerMovement : MonoBehaviour
 			vert = down;	
 		}
 		
-		move(hor, vert, zaxis);
-		
+		if (jumpInput == 1)
+		{
+			zaxis += 1;
+			movement.y = zaxis;
+		}
+
+		if(transform.position.y < 0)
+		{
+			movement.y = 0;
+			transform.position = movement;
+
+			zaxis = 0;
+		}
+
+		move(hor,zaxis, vert);
 	}
 
 	void Update () 
